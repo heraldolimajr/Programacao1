436b734aec6330ad0898ae0dcafdc8bff4fd3a3c
Heraldo Jr <heraldo.info@gmail.com>
2022-05-30 18:30:29 -0300

    Create TesteMath.java

---
+++ Códigos de Aula/TesteMath.java
@@ -0,0 +1,17 @@
+
+public class TesteMath {
+
+	public static void main(String[] args) {
+		double x = 2.0;
+		double y = -4.0;
+		double resultado, raiz;
+		
+		resultado = Math.pow(x, 3.0);
+		
+		System.out.println("Resultado:"+resultado);
+		System.out.println("Raiz: "+Math.sqrt(y));
+		System.out.println("Valor absoluto:"+Math.abs(y));
+
+	}
+
+}
